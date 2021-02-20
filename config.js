27ceec9fabf6b1e1b1f19378c9384e592a16b8d1
stan <stanleyscho@gmail.com>
2021-02-20 00:10:42 -0800

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '263508892008241',
+        clientSecret: '828df0060b2b958fa9d8e3a7427185f1'
+    }
 }
